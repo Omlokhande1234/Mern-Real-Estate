20922327d91672b883cf7c44ce6cd606829ee7c9
Omlokhande1234 <olokhande56@gmail.com>
2024-01-04 07:57:05 +0530

    Created pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
-import { useState } from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './Pages/Home'
+import SignOut from './Pages/SignOut'
+import About from './Pages/About'
+import Signin from './Pages/Signin'
+import Profile from './Pages/Profile'
 
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      
-       
-    </>
-  )
+export default function App(){
+  return <BrowserRouter>
+      <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/sign-in' element={<Signin/>}/>
+          <Route path='/sign-out' element={<SignOut/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/profile' element={<Profile/>}/>
+      </Routes>
+  
+       </BrowserRouter>
 }
 
-export default App
