c11e970bd6a62838c67bae5c2505840949451e2b
Omlokhande1234 <olokhande56@gmail.com>
2024-01-18 19:08:39 +0530

    added redux persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,13 +1,21 @@
 //We use the store in order to use the data given by the user in all the files and some functions
 //to be defined here and be used in all other files
 
-import { configureStore } from '@reduxjs/toolkit'
+import {combineReducers, configureStore } from '@reduxjs/toolkit'
 import userReducer from './user/userSlice.js'
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+const rootReducer = combineReducers({ user: userReducer });
 
+const persistConfig = {
+  key: 'root',
+  storage,
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 export const store = configureStore({
-  reducer: {
-    user:userReducer,
- },
+  reducer: persistedReducer,
   middleware:(getDefaultMiddleware)=>
   getDefaultMiddleware({
     serializableCheck:false
@@ -15,3 +23,4 @@ export const store = configureStore({
  
   
 })
+export const persistor = persistStore(store);
