043b1f65499759e561b36b7a706ce84642833729
Omlokhande1234 <olokhande56@gmail.com>
2024-01-31 20:27:19 +0530

    added search api route

--- Server/Routes/listingRoutes.js
+++ Server/Routes/listingRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createListing, deleteListings, updateListings } from "../Controllers/listingController.js";
+import { createListing, deleteListings, updateListings,getListings } from "../Controllers/listingController.js";
 import { verifyToken } from "../utils/verifyUser.js";
 import { getListing } from "../Controllers/listingController.js";
 const router=Router()
@@ -8,5 +8,5 @@ router.post('/create',verifyToken,createListing)
 router.delete('/delete/:id',verifyToken,deleteListings)
 router.post('/update/:id',verifyToken,updateListings)
 router.get('/get/:id',getListing)
-
+router.get('/get',getListings)
 export default router
